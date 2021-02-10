70c14a2f236bc7358e9969de9305ed41273ef2f0
Khang Nguyen <Khang.nguyen@student.csulb.edu>
2021-02-09 17:28:51 -0800

    modified

---
+++ ex1.js
@@ -0,0 +1,4 @@
+/* Tutorial 4
+Example 1 JaveScript code
+*/
+console.log("Khang's Output from Tutorial 4 Example 1");
---
+++ ex2.js
@@ -0,0 +1,4 @@
+/* Tutorial 4
+Example 2 JaveScript code
+*/
+console.log("Khang's Output from Tutorial 4 Example 2");
